cdb87efd28a94d705ecbefe2ce4b48f5d9820197
Silvia Martinez <sil@silvia.com>
2022-02-14 19:57:32 +0100

    Added component Task

---
+++ src/components/Task/Task.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const StyledTask = styled.li`
+  p {
+    text-decoration: none;
+    color: violet;
+  }
+  .done {
+    text-decoration: line-through;
+    color: blue;
+  }
+`;
+
+const Task = ({ id, name, done }) => {
+  return (
+    <StyledTask>
+      <p className={done ? "done" : ""}></p>
+
+      <p className="delete">X</p>
+    </StyledTask>
+  );
+};
+
+export default Task;
