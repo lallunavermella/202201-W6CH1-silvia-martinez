59d7bc06c8b8e35645101a5a1fd0bc273aba4a41
Silvia Martinez <sil@silvia.com>
2022-02-15 20:05:02 +0100

    Add thunk functionn delete

--- src/redux/thunks/thunks.js
+++ src/redux/thunks/thunks.js
@@ -1,8 +1,23 @@
-import { loadTasksActions } from "../actions/actionCreators";
+import { loadTasksActions, deleteTaskActions } from "../actions/actionCreators";
 
 export const loadTasksThunk = async (dispatch) => {
   const response = await fetch("https://todo-gatete.herokuapp.com/gatetes");
   const tasksList = await response.json();
 
   dispatch(loadTasksActions(tasksList));
+};
+
+export const deleteTaskThunk = (id) => async (dispatch) => {
+  const request = {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+  };
+  const response = await fetch(
+    `https://todo-gatete.herokuapp.com/gatetes${id}`,
+    request
+  );
+
+  const tasksList = await response.json();
+
+  dispatch(deleteTaskActions(tasksList));
 };
