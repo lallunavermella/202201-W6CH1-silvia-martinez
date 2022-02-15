92186363dee8ba05a4c77476cdc2132244a5843e
Silvia Martinez <sil@silvia.com>
2022-02-15 19:54:48 +0100

    Update component list test

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -1,20 +1,29 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import List from "./List";
-import { Provider } from "react-redux";
-import store from "../../redux/store";
+import renderWithProviders from "../../setupTests";
 
 describe("Given a List component", () => {
   describe("When called a list", () => {
     test("it shoul render a list of task", () => {
-      render(
-        <Provider store={store}>
-          <List />
-        </Provider>
-      );
+      renderWithProviders(<List />);
 
       const renderedElement = screen.getAllByRole("list");
 
       expect(renderedElement[0]).toBeInTheDocument();
+    });
+  });
+  describe("When it receives an array with objects", () => {
+    test("Then it should render the tasks", async () => {
+      const expectedFirstTask = /sleep all day/i;
+      const expectedSecondTask = /sleep in the sofa/i;
+
+      renderWithProviders(<List />);
+
+      const findFirstTask = await screen.findByText(expectedFirstTask);
+      const findSecondTask = await screen.findByText(expectedSecondTask);
+
+      expect(findFirstTask).toBeInTheDocument();
+      expect(findSecondTask).toBeInTheDocument();
     });
   });
 });
